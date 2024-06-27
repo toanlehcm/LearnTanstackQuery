1bc34571e21b7fb8118d7841f206b855c0af9896
Toan Le <thanhtoanhcm1602@gmail.com>
2024-06-27 11:27:10 +0700

    Tanstack Query 10: Dependent Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import { useProfile } from './hooks'
 
 const App: React.FC = () => {
-  const { data: userProfile, isLoading, isError, error } = useProfile()
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const { data: userProfile, isLoading, isError, error } = useProfile({ enabled: isLoggedIn })
 
   if (isLoading) {
     return <div>Loading...</div>
@@ -11,10 +13,15 @@ const App: React.FC = () => {
     return <div>Error: {error.message}</div>
   }
 
+  const toggleLogin = () => setIsLoggedIn(!isLoggedIn)
+
   return (
     <div>
       <h2>User Profile</h2>
-      {userProfile && (
+
+      <button onClick={toggleLogin}>Toggle Login</button>
+
+      {isLoggedIn && userProfile && (
         <>
           <p>Name: {userProfile?.name}</p>
           <p>Email: {userProfile?.email}</p>
